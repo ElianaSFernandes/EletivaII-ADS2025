3f40ccc293b61083d4f4c114d851c28b76ff1e9b
ElianaSFernandes <elianadesouzafernandes@gmail.com>
2025-08-26 21:44:20 -0300

    Exercício2

--- primeiroprojeto/app/Http/Controllers/PrimeiraController.php
+++ primeiroprojeto/app/Http/Controllers/PrimeiraController.php
@@ -13,11 +13,20 @@ class PrimeiraController extends Controller
     public function abrirForm(){
         return view ("exemplo");
     }
-
     public function resposta(Request $request){
         $valor1 = $request->valor1;
         $valor2 = $request->valor2;
         $soma = $valor1 + $valor2;
         return "Soma: ".$soma;
+    }
+
+    public function exercicio2(){
+        return view ("exercicio2");
+    }
+    public function respExercicio2(Request $request){
+        $valor1 = $request->valor1;
+        $valor2 = $request->valor2;
+        $sub = $valor1 - $valor2;
+        return "Subtração: ".$sub;
     }
 }
